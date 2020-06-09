4a21ffb35ab02850e7de3fda16bf83f4cd3b98a1
Kirill <petrovvkirill@gmail.com>
2020-06-09 19:58:29 +0300

    Updates

---
+++ precache-manifest.cd68cb7567afe5cec4411b84f072c51d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad20fcc80456932b8ccf48c11947a02c",
+    "url": "/react-material-table/index.html"
+  },
+  {
+    "revision": "c85a90e8526b10a3e1af",
+    "url": "/react-material-table/static/css/main.dc546cb1.chunk.css"
+  },
+  {
+    "revision": "21eef9518d4dde715dac",
+    "url": "/react-material-table/static/js/2.077ca46f.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/react-material-table/static/js/2.077ca46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85a90e8526b10a3e1af",
+    "url": "/react-material-table/static/js/main.4df1bb5f.chunk.js"
+  },
+  {
+    "revision": "0e2b08e110a8321aea19",
+    "url": "/react-material-table/static/js/runtime-main.f333ca56.js"
+  }
+]);
